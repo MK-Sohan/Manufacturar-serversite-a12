5e07714d6432ae872fb485741c23cb9cf8577547
MK-Sohan <mksohan786@gmail.com>
2022-05-22 19:14:20 +0600

    delete api create

--- index.js
+++ index.js
@@ -70,6 +70,15 @@ async function run() {
     });
 
     // order post section end==================
+    // delete order start=================
+    app.delete("/myorder/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // delete order end=================
   } finally {
   }
 }
